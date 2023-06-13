f4dbe2048b1b5f8eb363e502ab3869d0ff204add
opewole <136464831+opewole@users.noreply.github.com>
2023-06-13 14:40:43 +0100

    Add files via upload

---
+++ jstutoria.js
@@ -0,0 +1,21 @@
+var username = "";
+var password = "";
+username = prompt("Enter your username", "eg..chidi");
+password = prompt("Enter your password", "eg..chidi");
+if(username =="" && password=="setve"){
+  alert("please enter username")
+}else{
+  alert("Wrong username and password");
+}
+var day = Number.value;
+var da = Number.value;
+day= prompt();
+da=prompt();
+if(day && da !==""){
+document.write(day+ da);
+}else{
+  alert("Maths error");
+}
+var num  = 9;
+var num2 = 10;
+document.write(num + num2);
